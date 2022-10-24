582f93fd069f306682d958d8f7f7603e26da2ac9
Sweigalytics <neilsweigard@gmail.com>
2022-10-23 22:28:33 -0400

    Updated Lasso and Ridge Regression
    Corrected issue with predictor in training data

--- Code/R/Predict_QB_Salary.r
+++ Code/R/Predict_QB_Salary.r
@@ -27,7 +27,7 @@ df_qb_contracts <- df_qb_contracts %>% mutate(net_point_differential_change = as
 df_qb_contracts_hurts <- df_qb_contracts %>% filter(passer_player_id == '00-0036389')
 df_qb_contracts <- df_qb_contracts %>% filter(passer_player_id != '00-0036389')
 
-EDA
+# EDA
 
 ## Histograms of QB Features
 num_cols <- colnames(select_if(df_qb_contracts, is.numeric))
@@ -116,15 +116,16 @@ df_qb_contracts_test <- res$test[ , all_vars]
 
 # Training and Evaluating Predictive Models
 
-
-## Best Subsets (example here: https://bookdown.org/tpinto_home/Regularisation/best-subset-selection.html)
-
 ### Remove NAs
 df_qb_contracts_train_na_rm <- drop_na(df_qb_contracts_train)
 
 df_qbs_contracts_train.bglm <- df_qb_contracts_train_na_rm[ , names(df_qb_contracts_train_na_rm) %in% c(pred_vars, "inflated_apy")]
 
-regfit.full <- regsubsets(inflated_apy ~ ., data = df_qbs_contracts_train.bglm, method = "exhaustive", nvmax = length(pred_vars))
+num_vars <- length(pred_vars)
+
+
+## Fit Best Subsets
+regfit.full <- regsubsets(inflated_apy ~ ., data = df_qbs_contracts_train.bglm, method = "exhaustive", nvmax = num_vars)
 
 reg.summary <- summary(regfit.full)
 
@@ -157,119 +158,244 @@ for (i in models_to_eval){
   lst.apy[[i]] <- lm(form, df_qbs_contracts_train.bglm)
 }
 
-### Predict 
-apy.adjr2.preds <- predict(lst.apy[[max.adjr2]], df_qb_contracts_test)
-apy.cp.preds <- predict(lst.apy[[min.cp]], df_qb_contracts_test)
-apy.bic.preds <- predict(lst.apy[[min.bic]], df_qb_contracts_test)
+### Predict Best Subsets
+apy.bs.adjr2.preds <- predict(lst.apy[[max.adjr2]], df_qb_contracts_test)
+apy.bs.cp.preds <- predict(lst.apy[[min.cp]], df_qb_contracts_test)
+apy.bs.bic.preds <- predict(lst.apy[[min.bic]], df_qb_contracts_test)
 
-apy.adjr2.mse <- mean((apy.adjr2.preds - df_qb_contracts_test$inflated_apy)^2)
-apy.cp.mse <- mean((apy.cp.preds - df_qb_contracts_test$inflated_apy)^2)
-apy.bic.mse <- mean((apy.bic.preds - df_qb_contracts_test$inflated_apy)^2)
-
-c(apy.adjr2.mse, apy.cp.mse, apy.bic.mse)
-
-cbind(apy.adjr2.preds, apy.cp.preds, apy.bic.preds, df_qb_contracts_test$inflated_apy)
+mse.bs.adjr2 <- mean((df_qb_contracts_test$inflated_apy - apy.bs.adjr2.preds)^2)
+mse.bs.cp <- mean((df_qb_contracts_test$inflated_apy - apy.bs.cp.preds)^2)
+mse.bs.bic <- mean((df_qb_contracts_test$inflated_apy - apy.bs.bic.preds)^2)
 
 
-best.apy.model <- best.apy$BestModel
+## Fit Forward Stepwise
+regfit.fwd <- regsubsets(inflated_apy ~ ., df_qbs_contracts_train.bglm, method = "forward", nvmax = num_vars)
+reg.fwd.summary <- summary(regfit.fwd)
 
-best.preds.apy <- as.numeric(predict(best.apy.model, df_qb_contracts_test))
+par(mfrow = c(2 , 2))
+plot(reg.fwd.summary$rss, xlab = "Number of Variables", ylab = "RSS", type = "l")
 
-mean((best.preds.apy - df_qb_contracts_test$inflated_apy)^2)
+plot(reg.fwd.summary$adjr2, xlab = "Number of Variables", ylab = "Adjusted RSq", type = "l")
+max.adjr2 = which.max(reg.fwd.summary$adjr2)
+points(max.adjr2, reg.fwd.summary$adjr2[max.adjr2], col = "red", cex = 2, pch = 20)
+
+plot(reg.fwd.summary$cp, xlab = "Number of Variables", ylab = "Cp", type = "l")
+min.cp = which.min(reg.fwd.summary$cp)
+points(min.cp, reg.fwd.summary$cp[min.cp], col = "red", cex = 2, pch = 20)
+
+plot(reg.fwd.summary$bic, xlab = "Number of Variables", ylab = "BIC", type = "l")
+min.bic = which.min(reg.fwd.summary$bic)
+points(min.bic, reg.fwd.summary$bic[min.bic], col = "red", cex = 2, pch = 20)
+
+xvars <- dimnames(reg.fwd.summary$which)[[2]][-1]
+responsevar <- "inflated_apy"
+
+models_to_eval <- sort(unique(c(max.adjr2, min.cp, min.bic)))
+
+lst.fwd.apy <- vector("list", dim(reg.fwd.summary$which)[1])
+
+for (i in models_to_eval){
+  print(i)
+  id <- reg.fwd.summary$which[i, ]
+  form <- reformulate(xvars[which(id[-1])], responsevar, id[1])
+  lst.fwd.apy[[i]] <- lm(form, df_qbs_contracts_train.bglm)
+}
+
+### Predict Forward Stepwise
+apy.fwd.adjr2.preds <- predict(lst.fwd.apy[[max.adjr2]], df_qb_contracts_test)
+apy.fwd.cp.preds <- predict(lst.fwd.apy[[min.cp]], df_qb_contracts_test)
+apy.fwd.bic.preds <- predict(lst.fwd.apy[[min.bic]], df_qb_contracts_test)
+
+mse.fwd.adjr2 <- mean((df_qb_contracts_test$inflated_apy - apy.fwd.adjr2.preds)^2)
+mse.fwd.cp <- mean((df_qb_contracts_test$inflated_apy - apy.fwd.cp.preds)^2)
+mse.fwd.bic <- mean((df_qb_contracts_test$inflated_apy - apy.fwd.bic.preds)^2)
 
 
-df_qbs_contracts_train.bglm <- rename(df_qb_contracts_train_na_rm[ , names(df_qb_contracts_train_na_rm) %in% c(pred_vars, "years")], y=years)
-best.years <- bestglm(df_qbs_contracts_train.bglm, IC = "BIC", family=gaussian, method="exhaustive")
+## Backward Stepwise
+regfit.bwd <- regsubsets(inflated_apy ~ ., df_qbs_contracts_train.bglm, method = "backward", nvmax = num_vars)
+reg.bwd.summary <- summary(regfit.bwd)
 
-summary(best.years$BestModel)
+par(mfrow = c(2 , 2))
+plot(reg.bwd.summary$rss, xlab = "Number of Variables", ylab = "RSS", type = "l")
 
-df_qb_contracts_test_na_rm <- drop_na(df_qb_contracts_test)
+plot(reg.bwd.summary$adjr2, xlab = "Number of Variables", ylab = "Adjusted RSq", type = "l")
+max.adjr2 = which.max(reg.bwd.summary$adjr2)
+points(max.adjr2, reg.bwd.summary$adjr2[max.adjr2], col = "red", cex = 2, pch = 20)
 
-best.preds.apy <- predict(best.apy$BestModel, newx = df_qb_contracts_test_na_rm)
+plot(reg.bwd.summary$cp, xlab = "Number of Variables", ylab = "Cp", type = "l")
+min.cp = which.min(reg.bwd.summary$cp)
+points(min.cp, reg.bwd.summary$cp[min.cp], col = "red", cex = 2, pch = 20)
 
-mean((best.preds.apy - df_qb_contracts_test_na_rm$inflated_apy)^2)
+plot(reg.bwd.summary$bic, xlab = "Number of Variables", ylab = "BIC", type = "l")
+min.bic = which.min(reg.bwd.summary$bic)
+points(min.bic, reg.bwd.summary$bic[min.bic], col = "red", cex = 2, pch = 20)
 
-predict(best.years$BestModel, df_qb_contracts_hurts)
+xvars <- dimnames(reg.bwd.summary$which)[[2]][-1]
+responsevar <- "inflated_apy"
 
-## Ridge Regression/Lasso
+models_to_eval <- sort(unique(c(max.adjr2, min.cp, min.bic)))
+
+lst.bwd.apy <- vector("list", dim(reg.bwd.summary$which)[1])
+
+for (i in models_to_eval){
+  print(i)
+  id <- reg.bwd.summary$which[i, ]
+  form <- reformulate(xvars[which(id[-1])], responsevar, id[1])
+  lst.bwd.apy[[i]] <- lm(form, df_qbs_contracts_train.bglm)
+}
+
+### Predict Backward Stepwise
+apy.bwd.adjr2.preds <- predict(lst.bwd.apy[[max.adjr2]], df_qb_contracts_test)
+apy.bwd.cp.preds <- predict(lst.bwd.apy[[min.cp]], df_qb_contracts_test)
+apy.bwd.bic.preds <- predict(lst.bwd.apy[[min.bic]], df_qb_contracts_test)
+
+mse.bwd.adjr2 <- mean((df_qb_contracts_test$inflated_apy - apy.bwd.adjr2.preds)^2)
+mse.bwd.cp <- mean((df_qb_contracts_test$inflated_apy - apy.bwd.cp.preds)^2)
+mse.bwd.bic <- mean((df_qb_contracts_test$inflated_apy - apy.bwd.bic.preds)^2)
+
+
+
+## Lasso
+
+### Log transforming data because of skewness/curvature.
+df_qb_contracts_train_log <- cbind(as.data.frame(lapply(df_qb_contracts_train[, names(df_qb_contracts_train) %in% pred_vars], function(x) log(x - min(x, na.rm = TRUE) + 1))), df_qb_contracts_train$inflated_apy) %>% rename("inflated_apy" = "df_qb_contracts_train$inflated_apy")
+rownames(df_qb_contracts_train_log) <- rownames(df_qb_contracts_train)
+
+df_qb_contracts_test_log <- cbind(as.data.frame(lapply(df_qb_contracts_test[, names(df_qb_contracts_test) %in% pred_vars], function(x) log(x - min(x, na.rm = TRUE) + 1))), df_qb_contracts_test$inflated_apy) %>% rename("inflated_apy" = "df_qb_contracts_test$inflated_apy")
+rownames(df_qb_contracts_test_log) <- rownames(df_qb_contracts_test)
+
+par(mfrow = c(2, 2))
+plot(df_qb_contracts_train$primary_passing_tds_per_game, df_qb_contracts_train$inflated_apy)
+plot(df_qb_contracts_train$primary_rushing_tds_per_game, df_qb_contracts_train$inflated_apy)
+plot(df_qb_contracts_train$turnovers_per_attempt, df_qb_contracts_train$inflated_apy)
+plot(df_qb_contracts_train$days_to_hurts_game, df_qb_contracts_train$inflated_apy)
+
+plot(df_qb_contracts_train_log$primary_passing_tds_per_game, df_qb_contracts_train$inflated_apy)
+plot(df_qb_contracts_train_log$primary_rushing_tds_per_game, df_qb_contracts_train$inflated_apy)
+plot(df_qb_contracts_train_log$turnovers_per_attempt, df_qb_contracts_train$inflated_apy)
+plot(df_qb_contracts_train_log$days_to_hurts_game, df_qb_contracts_train$inflated_apy)
 
 ### Scaled versions of training and test data.
+
 normParam <- preProcess(df_qb_contracts_train[ , names(df_qb_contracts_train) %in% pred_vars])
 df_qbs_contracts_train_scaled <- predict(normParam, df_qb_contracts_train)
 df_qbs_contracts_test_scaled <- predict(normParam, df_qb_contracts_test)
 
-x.train <- data.matrix(subset(df_qbs_contracts_train_scaled))
-y.train <- data.matrix(df_qb_contracts_train[, c('inflated_apy')])
+x.train <- data.matrix(df_qbs_contracts_train_scaled[, names(df_qbs_contracts_train_scaled) %in% pred_vars])
+y.train <- data.matrix(df_qbs_contracts_train_scaled[, c('inflated_apy')])
 
 ### Storing the number of rows into a variable
 x.train.rows <- dim(x.train)[1]
 
 ### Fit with a LOOCV by defining the folds as the number of rows in the training data.
-grid <- 10^seq (10, -2, length = 100)
+grid <- 10^seq (10, -2, length = 1000)
 
 lasso.fit <- cv.glmnet(x.train, y.train, family = "gaussian", alpha = 1, lambda = grid, nfolds = x.train.rows)
 
+par(mfrow = c(1 , 1))
 plot(lasso.fit, xvar="lambda", label=TRUE)
 
-bestlam <- lasso.fit$lambda.min
+plot(lasso.fit$glmnet.fit, "norm", label=TRUE)
+plot(lasso.fit$glmnet.fit, "lambda", label=TRUE)
+
+bestlam.lasso.apy <- lasso.fit$lambda.min
 
 x.test <- data.matrix(subset(df_qbs_contracts_test_scaled, select = -c(inflated_apy)))
 y.test <- data.matrix(df_qbs_contracts_test_scaled[, c('inflated_apy')])
 
-lasso.preds <- predict(lasso.fit, s = bestlam, newx = x.test)
+lasso.preds <- predict(lasso.fit, s = bestlam.lasso.apy, newx = x.test)
 
-mean((lasso.preds - df_qb_contracts_test$inflated_apy)^2)
+mse.lasso.apy <- mean((lasso.preds - df_qb_contracts_test$inflated_apy)^2)
+
+coef(lasso.fit, s = bestlam.lasso.apy)
+
+
+#### Fitting and Predicting number of years on the contract (Lasso)
+x.train.years <- data.matrix(df_qbs_contracts_train_scaled[, names(df_qbs_contracts_train_scaled) %in% pred_vars])
+y.train.years <- data.matrix(df_qbs_contracts_train_scaled[, c('years')])
+
+x.train.rows <- dim(x.train.years)[1]
+
+grid <- 10^seq (10, -2, length = 1000)
+
+lasso.fit.years <- cv.glmnet(x.train.years, y.train.years, family = "gaussian", alpha = 1, lambda = grid, nfolds = x.train.rows)
+
+par(mfrow = c(1 , 1))
+plot(lasso.fit.years, xvar="lambda", label=TRUE)
+
+plot(lasso.fit.years$glmnet.fit, "norm", label=TRUE)
+plot(lasso.fit.years$glmnet.fit, "lambda", label=TRUE)
+
+bestlam <- lasso.fit.years$lambda.min
+
+x.test.years <- data.matrix(df_qbs_contracts_test_scaled[, names(df_qbs_contracts_test_scaled) %in% pred_vars])
+y.test.years <- data.matrix(df_qbs_contracts_test_scaled[, c('years')])
+
+lasso.preds.years <- predict(lasso.fit.years, s = bestlam, newx = x.test.years)
+
+mse.lasso.years <- mean((lasso.preds.years - df_qb_contracts_test$years)^2)
+
+coef(lasso.fit.years, s = bestlam)
+
+
+## Ridge Regression
+grid <- 10^seq (10, -2, length = 1000)
+rr.fit <- cv.glmnet(x.train, y.train, family = "gaussian", alpha = 0, lambda = grid, nfolds = x.train.rows)
+
+par(mfrow = c(1 , 1))
+plot(rr.fit, xvar="lambda", label=TRUE)
+
+plot(rr.fit$glmnet.fit, "norm", label=TRUE)
+plot(rr.fit$glmnet.fit, "lambda", label=TRUE)
+
+bestlam <- rr.fit$lambda.min
+
+x.test <- data.matrix(subset(df_qbs_contracts_test_scaled, select = -c(inflated_apy)))
+y.test <- data.matrix(df_qbs_contracts_test_scaled[, c('inflated_apy')])
+
+rr.preds <- predict(rr.fit, s = bestlam, newx = x.test)
+
+mse.rr <- mean((rr.preds - df_qb_contracts_test$inflated_apy)^2)
+
+coef(rr.fit, s = bestlam)
+
+
+#### Fitting and Predicting Number of Years on Contract (Ridge Regression)
+x.train.years <- data.matrix(df_qbs_contracts_train_scaled[, names(df_qbs_contracts_train_scaled) %in% pred_vars])
+y.train.years <- data.matrix(df_qbs_contracts_train_scaled[, c('years')])
+
+x.train.rows <- dim(x.train.years)[1]
+
+grid <- 10^seq (10, -2, length = 1000)
+
+rr.fit.years <- cv.glmnet(x.train.years, y.train.years, family = "gaussian", alpha = 0, lambda = grid, nfolds = x.train.rows)
+
+par(mfrow = c(1 , 1))
+plot(rr.fit.years, xvar="lambda", label=TRUE)
+
+plot(rr.fit.years$glmnet.fit, "norm", label=TRUE)
+plot(rr.fit.years$glmnet.fit, "lambda", label=TRUE)
+
+bestlam <- rr.fit.years$lambda.min
+
+x.test.years <- data.matrix(df_qbs_contracts_test_scaled[, names(df_qbs_contracts_test_scaled) %in% pred_vars])
+y.test.years <- data.matrix(df_qbs_contracts_test_scaled[, c('years')])
+
+rr.preds.years <- predict(rr.fit.years, s = bestlam, newx = x.test.years)
+
+mse.rr.years <- mean((rr.preds.years - df_qb_contracts_test$years)^2)
+
+coef(lasso.fit.years, s = bestlam)
 
 
 
+# Check Winning Model's Assumptions
+lasso.fit$lambda[bestlam]
 
-## XGBoost
-x.train <- data.matrix(subset(df_qbs_contracts_train, select = -c(apy)))
-y.train <- data.matrix(df_qbs_train[, c('franchise_qb')])
+cbind(apy.fwd.bic.preds,rr.preds, lasso.preds, df_qb_contracts_test$inflated_apy)
 
-x.test <- data.matrix(subset(df_qbs_test, select = -c(franchise_qb)))
-y.test <- data.matrix(df_qbs_test[, c('franchise_qb')])
+df_qbs_contracts_hurts_scaled <- predict(normParam, df_qb_contracts_hurts)
 
-dtrain <- xgb.DMatrix(data = x.train,label = y.train)
-dtest <- xgb.DMatrix(data = x.test, label = y.test)
+x.test.hurts <- data.matrix(df_qbs_contracts_hurts_scaled[, names(df_qbs_contracts_hurts_scaled) %in% pred_vars])
 
-hyper_grid <- expand.grid(max_depth = seq(2, 6, 1), eta = seq(.1, .4, .005))  
-
-xgb_train_logloss <- NULL
-xgb_test_logloss <- NULL
-
-for (j in 1:nrow(hyper_grid)) {
-  set.seed(581)
-  m_xgb_untuned <- xgb.cv(
-    data = dtrain,
-    nrounds = 1000,
-    objective = "binary:logistic",
-    early_stopping_rounds = 3,
-    nfold = x.train.rows,
-    max_depth = hyper_grid$max_depth[j],
-    eta = hyper_grid$eta[j]
-  )
-  
-  xgb_train_logloss[j] <- m_xgb_untuned$evaluation_log$train_logloss_mean[m_xgb_untuned$best_iteration]
-  xgb_test_logloss[j] <- m_xgb_untuned$evaluation_log$test_logloss_mean[m_xgb_untuned$best_iteration]
-  
-  cat(j, "\n")
-} 
-
-## Pulling the tuned parameters.
-best_max_depth <- hyper_grid[which.min(xgb_test_logloss), ]$max_depth
-best_eta <- hyper_grid[which.min(xgb_test_logloss), ]$eta
-
-set.seed(581)
-
-m1_xgb <-
-  xgboost(
-    data = dtrain,
-    nrounds = 1000,
-    objective = "binary:logistic",
-    early_stopping_rounds = 3,
-    max_depth = best_max_depth,
-    eta = best_eta
-  )   
-
-xg.pred <- predict(m1_xgb, dtest)
+predict(lasso.fit, s = bestlam.lasso.apy, newx = x.test.hurts)
